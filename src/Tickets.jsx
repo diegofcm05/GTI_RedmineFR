8b527d603c1c2d79f4bb1220479756cfe58de2df
JunniorSauceda <junniorgsauceda@gmail.com>
2025-12-15 20:33:20 -0600

    Header creado

---
+++ src/Tickets.jsx
@@ -0,0 +1,23 @@
+// Tickets.jsx
+import { useState } from "react";
+import "./Tickets.css";
+import NuevoReporte from "./components/NuevoReporte.jsx"
+
+export default function SistemaTickets() {
+  
+  return (
+    <>
+      {/* Header con título a la izquierda */}
+      <header className="tickets-header">
+        <div className="header-content">
+          <h1 className="tickets-title">Sistema de Tickets</h1>
+          <div className="header-divider"></div>
+        </div>
+      </header>
+      
+      {/* Contenido centrado */}
+        <NuevoReporte/>
+      
+    </>
+  );
+}
